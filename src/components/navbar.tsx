d8ef77f0c44a3d5d79c02b846b102976cb194537
SarthakPatil <125047555+SarthakPatil11@users.noreply.github.com>
2025-01-23 00:22:56 +0530

    ProjectDetails Page rending issue fixed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { AllOverviewLinks, AllProjectLinks } from "./navLinks";
+import { getWhatIDoData } from "@/actions/whatIDoData";
+import { useEffect, useState } from "react";
+import { iWhatIDoData } from "@/types/types";
 
 export default function Navbar() {
   const path = usePathname();
@@ -44,5 +46,62 @@ export default function Navbar() {
       </div>
       <div className="navbar-end">{/* <a className="btn ">Button</a> */}</div>
     </div>
+  );
+}
+
+export function AllOverviewLinks() {
+  return (
+    <>
+      <li className="text-1xl font-extrabold bg-gradient-to-r from-purple-start to-pink-end bg-clip-text text-transparent">
+        <a href="">WELCOME</a>
+      </li>
+      <li className="text-1xl font-extrabold text-slate-400">
+        <a href="#WhoIAm">WHO I AM</a>
+      </li>
+      <li className="text-1xl font-extrabold text-slate-400">
+        <a href="#WhatIDo">WHAT I DO</a>
+      </li>
+      <li className="text-1xl font-extrabold text-slate-400">
+        <a href="#Footer">GET IN TOUCH</a>
+      </li>
+    </>
+  );
+}
+
+export function AllProjectLinks() {
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [listWhatIDo, setListWhatIDo] =
+    useState<Array<Partial<iWhatIDoData>>>();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const data = await getWhatIDoData();
+        setListWhatIDo(data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.log(error);
+      }
+    })();
+  }, []);
+  // const listWhatIDo = await getWhatIDoData();
+
+  if (isLoading) {
+    return <>Loading.....</>;
+  }
+
+  return (
+    <>
+      <li className="text-1xl font-extrabold bg-gradient-to-r from-purple-start to-pink-end bg-clip-text text-transparent">
+        <a href="/application/user/overview">WELCOME</a>
+      </li>
+      {listWhatIDo?.map((v, i) => (
+        <li key={i} className="text-1xl font-extrabold text-slate-400">
+          <a href={`#${v.workTopic}`}>{v.workTopic}</a>
+        </li>
+      ))}
+    </>
   );
 }
